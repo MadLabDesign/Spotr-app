f57a8b4b4f6869bf52e6c0ea10c4753f2694b2a2
streetart <overloadedpixel@gmail.com>
2018-03-12 14:48:23 +0200

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule} from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-declare var jquery:any;
-declare var $ :any;
+import { animation } from '@angular/animations';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
 
@@ -15,17 +15,6 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
-    $(document).on("scroll", function(){
-      if
-      ($(document).scrollTop() > 0){
-        $("#homePage").addClass("shrink");
-      }
-      else
-      {
-        $("#homePage").removeClass("shrink");
-      }
-    });
-
   }
 
 }
